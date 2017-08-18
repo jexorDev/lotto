22d24c92d41f965fbd8192505a896c6755884d5b
jacobc <jtchadwell5@gmail.com>
2017-08-18 09:22:46 -0500

    Added some commented examples of Spring MVC URL mapping based on the dasboot example from Pluralsight

--- src/main/java/com/lotto/controller/HomeController.java
+++ src/main/java/com/lotto/controller/HomeController.java
@@ -4,10 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping("api/v1/")
 public class HomeController {
 
     @RequestMapping("/")
+    //Method-level request mapping appends its value onto the end of the class-level annotation
+    //@RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
     public String home() {
         return "YAAAAY";
     }
+
+    // another example with a specific id on the URL
+    // @RequestMapping(value = shipwrecks/{id}", method = RequestMethod.PUT)
+    // public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck) }
+    //     return ShipwreckStub.update(id, shipwreck);
+    // }
 }
