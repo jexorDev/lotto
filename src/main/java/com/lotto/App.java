81b7e34ca9d7c6f03788c5e6a5185a6f67aec544
jacobc <jtchadwell5@gmail.com>
2017-08-18 08:38:53 -0500

    Changed hello world file to a spring boot application

--- src/main/java/com/lotto/App.java
+++ src/main/java/com/lotto/App.java
@@ -1,13 +1,19 @@
 package com.lotto;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+
+// Auto-configure spring application (scans app for spring components and wires up libraries
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
