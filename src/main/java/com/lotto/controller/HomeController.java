3469d39da6d708e9e7a7fa1e02e880f7935c8302
thecapn2k5 <thecapn.morgan@gmail.com>
2017-08-18 14:09:04 -0500

    /home uses template and Thymeleaf. / redirects to /home

--- src/main/java/com/lotto/controller/HomeController.java
+++ src/main/java/com/lotto/controller/HomeController.java
@@ -1,17 +1,20 @@
 package com.lotto.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 //@RequestMapping("api/v1/")
 public class HomeController {
 
-    @RequestMapping("/home.html")
+    @RequestMapping("/home")
     //Method-level request mapping appends its value onto the end of the class-level annotation
     //@RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
-    public String home() {
-        return "hello from home!";
+    public Model home(Model model) {
+        model.addAttribute("name", "jacob");
+        return model;
     }
 
     // another example with a specific id on the URL
