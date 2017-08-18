0c082a3f3bc3d6cf2b0d31cfda6a07f0b6b7add4
jacobc <jtchadwell5@gmail.com>
2017-08-18 12:59:42 -0500

    Home example added

--- src/main/java/com/lotto/controller/HomeController.java
+++ src/main/java/com/lotto/controller/HomeController.java
@@ -3,11 +3,11 @@ package com.lotto.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 //@RequestMapping("api/v1/")
 public class HomeController {
 
-    //@RequestMapping("/home.html")
+    @RequestMapping("/home.html")
     //Method-level request mapping appends its value onto the end of the class-level annotation
     //@RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
     public String home() {
